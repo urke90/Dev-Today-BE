70b790ecd28a385b08963ed3c6a671be5a0d23ad
MarkoTodorovic <markec026@yahoo.com>
2024-07-01 15:42:49 +0200

    feat: Add endpoint for retrieving content statistics and sidebar details

--- src/types/content.ts
+++ src/types/content.ts
@@ -3,3 +3,33 @@ export enum EContentTypeLowercase {
   MEETUP = 'meetup',
   PODCAST = 'podcast',
 }
+export enum EPageTypeLowercase {
+  POSTS = 'posts',
+  MEETUPS = 'meetups',
+  PODCASTS = 'podcasts',
+}
+
+export type CommonData = {
+  popularTagsSorted: { id: string; title: string; count: number }[];
+  popularGroupsSorted: { id: string; name: string; count: number }[];
+  popularPosts?: {
+    id: string;
+    title: string;
+    coverImage: string | null;
+    author: { userName: string };
+  }[];
+  popularMeetups?: {
+    id: string;
+    title: string;
+    coverImage: string | null;
+    tags: { id: string; title: string }[];
+    meetupLocation: string | null;
+    meetupDate: Date | null;
+  }[];
+  podcasts?: {
+    id: string;
+    title: string;
+    coverImage: string | null;
+    author: { userName: string };
+  }[];
+};
